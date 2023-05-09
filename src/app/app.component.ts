1694c99d6b074ce7586b4bb805b1b788dc9dd73f
amitpatni13 <amitpatni13@gmail.com>
2023-05-09 14:24:56 +0530

    Portfolio app name corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'andresjosehr-portfolio';
+  title = 'portfolio';
   
   constructor(
     private titleService: Title,
